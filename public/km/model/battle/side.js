f56ee1f7fc93acda326d78264de45a848ef829b9
Yury Pozdnyakov <ypozdnyakov@issart.com>
2011-08-06 13:59:28 +0700

    [KM-4] - use model base class for battle.side

--- public/km/model/battle/side.js
+++ public/km/model/battle/side.js
@@ -1,6 +1,6 @@
 JW.ns("KM.Model.Battle");
 
-KM.Model.Battle.Side = JW.Class.extend({
+KM.Model.Battle.Side = JW.Model.extend({
     inPower             : null,         //[required] Integer
 
     outPower            : null,         //[readonly] Integer
