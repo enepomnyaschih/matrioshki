9e7bf9db44fbef2e2fefb4fcb205672ab0025bd4
Yury Pozdnyakov <ypozdnyakov@issart.com>
2011-08-06 19:06:57 +0700

    [KM-4] - calculate new power

--- public/km/ui/game/status/endTurn.js
+++ public/km/ui/game/status/endTurn.js
@@ -13,7 +13,67 @@ KM.UI.Game.Status.EndTurn = KM.UI.Game.Status.extend({
     
     _calculatePower: function()
     {
-        return 10;
+        var areas   = this.gameView.game.map.areas;
+        var n       = areas.length;
+        var cur     = 0;
+        var was     = [];
+        var queue   = [];
+        var maxC    = 0;
+        
+        for (var i = 0; i < n; i++)
+        {
+            was[i] = -1;
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            var area = areas[i];
+
+            if (was[i] != -1 || area.player != this.gameView.currentPlayer)
+               continue;
+            
+            queue.push(i);
+            
+            while (queue.length)
+            {
+                var v = queue.shift();
+                if (was[v] != -1)
+                    continue;
+                
+                was[v] = cur;
+                var area1 = areas[v];
+                
+                JW.each (area1.borders, function(j) {
+                    var area2 = areas[j];
+                    if (was[j] == -1 && area2.player == area1.player)
+                    {
+                        queue.push(j);
+                    }
+                });
+            }
+            ++cur;
+        }
+
+        var result = 1;
+        
+        for (var i = 0; i < n; i++)
+        {
+            var area = areas[i];
+
+            if (area.player != this.gameView.currentPlayer)
+               continue;
+
+            var temp = 0;
+
+            for (var j = 0; j < n; j++)
+            {
+                temp += (was[j] == i);
+            }
+
+            result = Math.max(result, temp);
+        }
+
+        return result;
     },
     
     _checkPower: function()
